4be5a6640d4d63f8c8313b9b01e4a73fda50a69f
rahmanlana <rahmanlana044@gmail.com>
2026-04-18 14:59:28 +0700

    feat(model): add Level model with fillable fields and user relation

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Level extends Model
 {
     //
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+    protected $fillable = ['level_kode', 'level_nama'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'level_id', 'level_id');
+    }
 }
