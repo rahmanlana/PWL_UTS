0658d2d08f12102495673b1086e9d38a3a6b4e9c
rahmanlana <rahmanlana044@gmail.com>
2026-04-18 15:06:30 +0700

    feat(model): add Supplier model with fillable fields and stok relation

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $table = 'm_supplier';
+    protected $primaryKey = 'supplier_id';
+    protected $fillable = ['supplier_kode', 'supplier_nama', 'supplier_alamat'];
+
+    public function stok()
+    {
+        return $this->hasMany(Stok::class, 'supplier_id', 'supplier_id');
+    }
 }
